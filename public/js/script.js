af9f4e0d97c9b16d2c1e26e5066165a9299a53ee
Kaung Htun Thant <3199busi@gmail.com>
2023-09-30 13:48:57 +0630

    Audio autoplay on mobile

--- public/js/script.js
+++ public/js/script.js
@@ -20,9 +20,10 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
-
-// document.getElementById("my_audio").play();
-// console.log('Shaadi me zaroor aana');
+$(document).on('click', function(){
+    document.getElementById("my_audio").play();
+    // console.log('Shaadi me zaroor aana');
+});
 
 // Set the date we're counting down to
 var countDownDate = new Date("Nov 25, 2023 00:00:00").getTime();
