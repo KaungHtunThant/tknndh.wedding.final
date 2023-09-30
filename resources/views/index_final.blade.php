cadc79edf8aa4f0d6c07e90feae1f086eaca7260
Kaung Htun Thant <3199busi@gmail.com>
2023-09-30 13:43:25 +0630

    Music autoplay fix

--- resources/views/index_final.blade.php
+++ resources/views/index_final.blade.php
@@ -222,7 +222,7 @@
 	         </p>
 		</div>
 	</section>
-	<audio src="./assets/mp3/song2.m4a" id="my_audio" loop="loop"></audio> 
+	<embed src="{{ url('assets/mp3/song2.m4a') }}" loop="true" autostart="true" width="2" height="0">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
 	<script src='https://cdn.jsdelivr.net/gh/timoschaefer/jQuery-Sakura/jquery-sakura.min.js'></script>
